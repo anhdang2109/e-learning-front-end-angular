ed97b2f5f8c9f2576e470a898f484c8e319f4639
Nguyễn Bùi Quang Minh <67447333+minhnguyen8219@users.noreply.github.com>
2021-01-14 11:19:24 +0700

    add modules of attempt, study, assumption

--- src/app/modules/admin_content/studies/study.module.ts
+++ src/app/modules/admin_content/studies/study.module.ts
@@ -4,10 +4,42 @@ import { StudyCreateComponent } from './study-create/study-create.component';
 import { StudyEditComponent } from './study-edit/study-edit.component';
 import { StudyImportComponent } from './study-import/study-import.component';
 import { StudyViewComponent } from './study-view/study-view.component';
+import {RouterModule, Routes} from "@angular/router";
+import {StudiesComponent} from "./studies.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: StudiesComponent
+  },
+  {
+    path: 'add',
+    component: StudyCreateComponent
+  },
+  {
+    path: 'edit',
+    component: StudyEditComponent
+  },
+  {
+    path: 'view',
+    component: StudyEditComponent
+  },
+  {
+    path: 'import',
+    component: StudyImportComponent
+  }
+];
 
 @NgModule({
-  declarations: [StudyCreateComponent, StudyEditComponent, StudyImportComponent, StudyViewComponent],
+  declarations: [
+    StudiesComponent,
+    StudyCreateComponent,
+    StudyEditComponent,
+    StudyImportComponent,
+    StudyViewComponent
+  ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
