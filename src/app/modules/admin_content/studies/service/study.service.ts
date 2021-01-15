bc2d49f7c6a39378f422ce84143d7b59eca9c241
Nguyễn Bùi Quang Minh <67447333+minhnguyen8219@users.noreply.github.com>
2021-01-15 10:53:42 +0700

    add study.model

--- src/app/modules/admin_content/studies/service/study.service.ts
+++ src/app/modules/admin_content/studies/service/study.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+
+const API = environment.API_FAKE;
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class StudyService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }
---
+++ src/app/modules/admin_content/studies/study.model.ts
@@ -0,0 +1,10 @@
+export interface Study {
+  id?: string;
+  studyname?: string;
+  description?: string;
+  highestScore?: string;
+  created_at?: string;
+  updated_at?: string;
+  quiz?: string;
+  // attempts: []
+}
